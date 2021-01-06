88e49904fc8cc842f6860f52f3132a05b6a621cd
Cássio Simões de Lemos Moreira <slm.cassio@gmail.com>
2021-01-06 09:09:14 -0300

    Add function to add and retract an entity

--- src/datomic_examples/query.clj
+++ src/datomic_examples/query.clj
@@ -2,13 +2,43 @@
   (:require [datomic.api :as d]
             [datomic-examples.rule :as d-e.rule]))
 
-(def uri "datomic:dev://localhost:4334/mbrainz-1968-1973")
-(def conn (d/connect uri))
-(def db (d/db conn))
+(defn new-artist
+  [name type gender start-year conn]
+  (let [temp-id (d/tempid :db.part/user)
+        gid     (java.util.UUID/randomUUID)]
+    (d/transact conn [[:db/add temp-id :artist/gid gid]
+                      [:db/add temp-id :artist/name name]
+                      [:db/add temp-id :artist/sortName name]
+                      [:db/add temp-id :artist/type type]
+                      [:db/add temp-id :artist/gender gender]
+                      [:db/add temp-id :artist/startYear start-year]])))
 
-(defn find-artist-info-by-name
+(defn artist-id-by-name
+  [name db]
+  (-> (d/q '[:find ?artist
+             :in $ ?artist-name
+             :where [?artist :artist/name ?artist-name]]
+           db
+           name)
+      first
+      first))
+
+(defn retract-artist-by-name
+  [name conn]
+  (let [artist-id (artist-id-by-name name (d/db conn))]
+    (d/transact conn [[:db/retractEntity artist-id]])))
+
+(defn all-artists
+  [db]
+  (d/q '[:find (pull ?artist [* {:artist/type [*]}])
+         :in $
+         :where
+         [?artist :artist/name ?artist-name]]
+       db))
+
+(defn artist-info-by-name
   "What is the type and gender of an artist?"
-  [artist-name]
+  [artist-name db]
   (d/q '[:find ?id ?type ?gender
          :in $ ?artist-name
          :where
@@ -21,9 +51,9 @@
        db
        artist-name))
 
-(defn find-tracks-by-artist
+(defn tracks-by-artist
   "What are the titles of all the tracks an artist played on?"
-  [artist-name]
+  [artist-name db]
   (d/q '[:find ?title
          :in $ % ?artist-name
          :where
@@ -33,9 +63,9 @@
        [d-e.rule/artist-track]
        artist-name))
 
-(defn find-track-info-by-artist
+(defn track-info-by-artist
   "What are the titles, album names, and release years of an artist tracks?"
-  [artist-name]
+  [artist-name db]
   (d/q '[:find ?title ?album ?year
          :in $ % ?artist-name
          :where
@@ -45,9 +75,9 @@
        [d-e.rule/artist-track d-e.rule/track-info]
        artist-name))
 
-(defn find-track-info-by-artist-before-year
+(defn track-info-by-artist-before-year
   "What are the titles, album names, and release years of an artist tracks?"
-  [artist-name year-limit]
+  [artist-name year-limit db]
   (d/q '[:find ?title ?album ?year
          :in $ % ?artist-name ?year-limit
          :where
@@ -59,21 +89,21 @@
        artist-name
        year-limit))
 
-(defn find-track-info-with-string
-    "What are the titles, artists, album names, and release years of all tracks
-     having the given word in their titles?"
-    [search]
-    (d/q '[:find ?title ?artist ?album ?year
-           :in $ % ?search
-           :where
-           (track-search ?search ?track)
-           (track-info ?track ?title ?artist ?album ?year)]
-         db
-         [d-e.rule/track-search d-e.rule/track-info]
-         search))
+(defn track-info-with-string
+  "What are the titles, artists, album names, and release years of all tracks
+   having the given word in their titles?"
+  [search db]
+  (d/q '[:find ?title ?artist ?album ?year
+         :in $ % ?search
+         :where
+         (track-search ?search ?track)
+         (track-info ?track ?title ?artist ?album ?year)]
+       db
+       [d-e.rule/track-search d-e.rule/track-info]
+       search))
 
-(defn find-release-info-by-artist
-  [artist-name]
+(defn release-info-by-artist
+  [artist-name db]
   (d/q '[:find ?album ?year (distinct ?artist-names)
          :in $ % ?artist-name
          :where
--- test/datomic_examples/query_test.clj
+++ test/datomic_examples/query_test.clj
@@ -1,35 +1,69 @@
 (ns datomic-examples.query-test
   (:require [clojure.test :refer :all]
+            [datomic.api :as d]
             [datomic-examples.query :as d-e.query]))
 
-(deftest find-artist-info-by-name-test
+(def uri "datomic:dev://localhost:4334/mbrainz-1968-1973")
+(def conn (d/connect uri))
+(def db (d/db conn))
+
+(def artist-name "Rich Rickey")
+(def artist-type :artist.type/person)
+(def artist-gender :artist.gender/male)
+(def artist-start-year 2007)
+
+(deftest new-artist-test
+  (testing "An artist can be added"
+    (do
+      (d-e.query/new-artist artist-name artist-type artist-gender artist-start-year conn)
+
+      (is (= #{}
+             (d-e.query/artist-info-by-name artist-name db)))
+
+      (is (some? (d-e.query/artist-id-by-name artist-name (d/db conn)))))))
+
+(deftest retract-artist-test
+  (testing "An artist can be retracted"
+    (do
+      (is (some? (d-e.query/artist-id-by-name artist-name (d/db conn))))
+
+      (d-e.query/retract-artist-by-name artist-name conn)
+
+      (is (= #{}
+             (d-e.query/artist-info-by-name artist-name (d/db conn)))))))
+
+(deftest all-artists-test
+  (testing "All artists can be found"
+    (is (not-empty (d-e.query/all-artists db)))))
+
+(deftest artist-info-by-name-test
   (testing "An artist can be found by name"
     (is (= #{[#uuid"76c9a186-75bd-436a-85c0-823e3efddb7f"
               :artist.type/person
               :artist.gender/female]}
-           (d-e.query/find-artist-info-by-name "Janis Joplin")))))
+           (d-e.query/artist-info-by-name "Janis Joplin" db)))))
 
-(deftest find-tracks-by-artist-test
+(deftest tracks-by-artist-test
   (testing "Track titles can be found by the artist name"
     (is (= ["Wicked World"]
-           (-> (d-e.query/find-tracks-by-artist "Black Sabbath")
+           (-> (d-e.query/tracks-by-artist "Black Sabbath" db)
                sort
                last)))
 
     (is (= 50
-           (count (d-e.query/find-tracks-by-artist "Black Sabbath"))))))
+           (count (d-e.query/tracks-by-artist "Black Sabbath" db))))))
 
-(deftest find-track-info-by-artist-test
+(deftest track-info-by-artist-test
   (testing "Track title, album and release years can be found by the artist name"
     (is (= ["Wrote a Song for Everyone" "Green River" 1969]
-           (-> (d-e.query/find-track-info-by-artist "Creedence Clearwater Revival")
+           (-> (d-e.query/track-info-by-artist "Creedence Clearwater Revival" db)
                sort
                last)))
 
     (is (= 102
-           (count (d-e.query/find-track-info-by-artist "Creedence Clearwater Revival"))))))
+           (count (d-e.query/track-info-by-artist "Creedence Clearwater Revival" db))))))
 
-(deftest find-track-info-by-artist-before-year-test
+(deftest track-info-by-artist-before-year-test
   (testing "Track title, album and release years can be found by the artist name before a given year"
     (is (= #{["Gloomy" "Creedence Clearwater Revival" 1968]
              ["The Working Man" "Creedence Clearwater Revival" 1968]
@@ -41,21 +75,21 @@
              ["Get Down Woman" "Creedence Clearwater Revival" 1968]
              ["Suzie Q" "Creedence Clearwater Revival" 1968]
              ["Walk on the Water" "Creedence Clearwater Revival" 1968]}
-           (d-e.query/find-track-info-by-artist-before-year "Creedence Clearwater Revival" 1969)))))
+           (d-e.query/track-info-by-artist-before-year "Creedence Clearwater Revival" 1969 db)))))
 
-(deftest find-track-info-with-string-test
+(deftest track-info-with-string-test
   (testing "Track title, artist, album and release years can be found a title word"
     (is (= ["You're Always Welcome to Our House" "The Clancy Brothers" "Welcome to Our House" 1970]
-           (-> (d-e.query/find-track-info-with-string "always")
+           (-> (d-e.query/track-info-with-string "always" db)
                sort
                last)))
 
     (is (= 92
-           (count (d-e.query/find-track-info-with-string "always"))))))
+           (count (d-e.query/track-info-with-string "always" db))))))
 
-(deftest find-release-info-by-artist-test
+(deftest release-info-by-artist-test
   (testing "Track title, album and release years can be found by the artist name"
     (is (= [["Another Day / Oh Woman Oh Why" 1971 #{"Paul McCartney"}]
             ["McCartney" 1970 #{"Paul McCartney"}]
             ["Ram" 1971 #{"Paul McCartney" "Linda McCartney"}]]
-           (d-e.query/find-release-info-by-artist "Paul McCartney")))))
+           (d-e.query/release-info-by-artist "Paul McCartney" db)))))
